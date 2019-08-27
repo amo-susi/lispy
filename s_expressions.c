c224691177fec5a4a72c5e2fe509d583cd62ea90
a-nano <an74abc@gmail.com>
2019-08-27 14:13:00 +0900

    added

--- s_expressions.c
+++ s_expressions.c
@@ -6,8 +6,7 @@ static char buffer[2048];
 
 char* readline(char* prompt) {
   fputs(prompt, stdout);
-  fputs(prompt, stdout);
-  fgets(buffer, stdout);
+  fgets(buffer, 2048, stdin);
   char* cpy = malloc(strlen(buffer)+1);
   strcpy(cpy, buffer);
   cpy[strlen(cpy)-1]= '\0';
@@ -53,7 +52,7 @@ lval* lval_err(char* m) {
 }
 
 /* Construct a pointer to a new Symbol lval */
-lval* lval_sys(char* s) {
+lval* lval_sym(char* s) {
   lval* v = malloc(sizeof(lval));
   v->type = LVAL_SYM;
   v->sym = malloc(strlen(s) + 1);
@@ -125,7 +124,7 @@ lval* lval_take(lval* v, int i) {
 
 void lval_print(lval* v);
 
-void lval_expr_print(lval* v, vhar open, char close) {
+void lval_expr_print(lval* v, char open, char close) {
   putchar(open);
   for(int i = 0; i < v->count; i++) {
 
@@ -195,4 +194,112 @@ lval* builtin_op(lval* a, char* op) {
   /* Delete input expression and return result */
   lval_del(a);
   return x;
+}
+
+lval* lval_eval(lval* v);
+
+lval* lval_eval_sexpr(lval* v) {
+
+  /* Evaluate Children */
+  for(int i = 0; i < v->count; i++) {
+    v->cell[i] = lval_eval(v->cell[i]);
+  }
+
+  /* Empty Expression */
+  if(v->count == 0) { return v; }
+
+  /* Single Expression */
+  if(v->count == 1) { return lval_take(v, 0); }
+
+  /* Ensure First Element is Symbol */
+  lval* f = lval_pop(v, 0);
+  if(f->type != LVAL_SYM) {
+    lval_del(f); lval_del(v);
+    return lval_err("S-expression Does not start with symbol.");
+  }
+
+  /* Call builtin with operator */
+  lval* result = builtin_op(v, f->sym);
+  lval_del(f);
+  return result;
+}
+
+lval* lval_eval(lval* v) {
+  /* Evaluate Sexpressions */
+  if(v->type == LVAL_SEXPR) { return lval_eval_sexpr(v); }
+  /* All other lval types remain the same */
+  return v;
+}
+
+lval* lval_read_num(mpc_ast_t* t) {
+  errno = 0;
+  long x = strtol(t->contents, NULL, 10);
+  return errno != ERANGE ?
+    lval_num(x) : lval_err("invalid number");
+}
+
+lval* lval_read(mpc_ast_t* t) {
+
+  /* If Symbol or Number return conversion to that type */
+  if(strstr(t->tag, "number")) { return lval_read_num(t); }
+  if(strstr(t->tag, "symbol")) { return lval_sym(t->contents); }
+
+  /* If root (>) or sexpr then create empty list */
+  lval* x = NULL;
+  if(strcmp(t->tag, ">") == 0) { x = lval_sexpr(); }
+  if(strstr(t->tag, "sexpr")) { x = lval_sexpr(); }
+  
+  /* Fill this list with any valid expression contained within */
+  for(int i = 0; i < t->children_num; i++) {
+    if(strcmp(t->children[i]->contents, "(") == 0) { continue; }
+    if(strcmp(t->children[i]->contents, ")") == 0) { continue; }
+    if(strcmp(t->children[i]->tag, "regex") == 0) { continue; }
+    x = lval_add(x, lval_read(t->children[i]));
+  }
+
+  return x;
+}
+
+int main(int argc, char** argv) {
+
+  mpc_parser_t* Number = mpc_new("number");
+  mpc_parser_t* Symbol = mpc_new("symbol");
+  mpc_parser_t* Sexpr = mpc_new("sexpr");
+  mpc_parser_t* Expr = mpc_new("expr");
+  mpc_parser_t* Lispy = mpc_new("lispy");
+
+  mpca_lang(MPCA_LANG_DEFAULT,
+	    "                                   \
+             number : /-?[0-9]+/ ;              \
+             symbol : '+' | '-' | '*' | '/' ;   \
+             sexpr : '(' <expr>* ')' ;          \
+             expr : <number> | <symbol> | <sexpr> ; \
+             lispy : /^/ <expr>* /$/ ;          \
+            ",
+	    Number, Symbol, Sexpr, Expr, Lispy);
+
+  puts("Lispy Version 0.0.0.0.5");
+  puts("Press Ctrl+c to Exit\n");
+
+  while(1) {
+
+    char* input = readline("lispy> ");
+    add_history(input);
+
+    mpc_result_t r;
+    if( mpc_parse("<stdin>", input, Lispy, &r)) {
+      lval* x = lval_eval(lval_read(r.output));
+      lval_println(x);
+      lval_del(x);
+      mpc_ast_delete(r.output);
+    } else {
+      mpc_err_print(r.error);
+      mpc_err_delete(r.error);
+    }
+    free(input);
+  }
+
+  mpc_cleanup(5, Number, Symbol, Sexpr, Expr, Lispy);
+
+  return 0;
 }
